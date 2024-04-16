af5883d26542eb37217c1473cd1d794d6f04b5db
Eli <elayjahh7@hotmail.com>
2024-04-16 12:14:51 +1200

    Xfer to PC

---
+++ src/DictionaryLookup.java
@@ -0,0 +1,16 @@
+public class DictionaryLookup {
+    String smallDicOrder = "../smallDicOrder.txt";
+    String largeDicOrder = "../largeDicOrder.txt";
+    String smallDick = "../smallDic.txt";
+    String largeDick = "../largeDic.txt";
+    public static void main(String[] args) {
+
+
+    } // end void
+
+    public void loadData() {
+
+
+    } // end void
+
+} // end class
